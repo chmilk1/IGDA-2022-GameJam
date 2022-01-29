ed20018db0220756b5a2b0a77fc4803b3b8a95cb
chmilk1(Home) <jamesmaron87@gmail.com>
2022-01-29 13:38:55 -0800

    Bullets and Incomplete Lasers

--- IGDAJam/Assets/Scripts/Entities/DamageTrigger.cs
+++ IGDAJam/Assets/Scripts/Entities/DamageTrigger.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.Events;
 
 namespace Entities
 {
@@ -7,14 +8,18 @@ namespace Entities
     {
         [SerializeField] private LayerMask targetLayers;
         [SerializeField] private int damage;
-        
+
+        [SerializeField] private UnityEvent onHit;
+
         private void OnTriggerEnter2D(Collider2D other)
         {
             bool isTargetLayer = targetLayers.value == (targetLayers.value | (1 << other.gameObject.layer));
             bool hasHealth = other.TryGetComponent(out Health health);
-            
-            if (isTargetLayer && hasHealth)
+
+            if (isTargetLayer && hasHealth) {
                 health.Damage(damage);
+                onHit.Invoke();
+            }
         }
     }
 }
--- IGDAJam/Assets/Settings/Input/Controls.cs
+++ IGDAJam/Assets/Settings/Input/Controls.cs
@@ -1,4 +1,4 @@
-// GENERATED AUTOMATICALLY FROM 'Assets/Controls.inputactions'
+// GENERATED AUTOMATICALLY FROM 'Assets/Settings/Input/Controls.inputactions'
 
 using System;
 using System.Collections;
