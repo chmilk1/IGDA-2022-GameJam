5c724aa0efe967caf522122b0182ee10059c331c
chmilk1(Home) <jamesmaron87@gmail.com>
2022-01-29 13:50:58 -0800

    Laser and Laser Prefab

--- IGDAJam/Assets/Scripts/Entities/LaserLogic.cs
+++ IGDAJam/Assets/Scripts/Entities/LaserLogic.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class LaserLogic : MonoBehaviour
 {
@@ -12,9 +13,26 @@ public class LaserLogic : MonoBehaviour
     [SerializeField] BoxCollider2D laserCollider;
     [SerializeField] GameObject laserSprite;
     [SerializeField] GameObject activeLaserSprite;
+    [SerializeField] private UnityEvent onLaserEnd;
 
-    private void Awake()
+    private IEnumerator Start()
     {
-        
+        yield return new WaitForSeconds(warmupTime);
+        activateLaser();
+        yield return new WaitForSeconds(activeTime);
+        onLaserEnd.Invoke();
+
+    }
+
+    public void distroyThis()
+    {
+        Destroy(this.gameObject);
+    }
+
+    private void activateLaser()
+    {
+        laserSprite.SetActive(false);
+        activeLaserSprite.SetActive(true);
+        laserCollider.enabled = true;
     }
 }
