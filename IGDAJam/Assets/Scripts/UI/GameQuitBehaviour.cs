a5562dbfba4bd52684412e523534da59ff2778e2
Daniel Walls <walls.dk@gmail.com>
2022-01-29 11:09:42 -0800

    added temp menu behaviour

---
+++ IGDAJam/Assets/Scripts/UI/GameQuitBehaviour.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace UI
+{
+    public class GameQuitBehaviour : MonoBehaviour
+    {
+        public void QuitGame()
+        {
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+#endif
+            Application.Quit();
+        }
+    }   
+}
---
+++ IGDAJam/Assets/Scripts/UI/GameStartBehaviour.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace UI
+{
+    public class GameStartBehaviour : MonoBehaviour
+    {
+        [SerializeField] private string gameplaySceneName;
+
+        // todo: add logic for loading a save (which will probably replace the need for gameplaySceneName
+        // todo: screen transition over async load
+        
+        public void StartGame()
+        {
+            SceneManager.LoadScene(gameplaySceneName);
+        }
+    }
+}
