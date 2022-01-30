7b0014113a541257e2ec01591307099474bfe040
chmilk1(Home) <jamesmaron87@gmail.com>
2022-01-30 00:45:00 -0800

    Updated Prefabs

--- IGDAJam/Assets/Scripts/Entities/ParentingEmmiter.cs
+++ IGDAJam/Assets/Scripts/Entities/ParentingEmmiter.cs
@@ -8,6 +8,7 @@ public class ParentingEmmiter : MonoBehaviour
 
     public void fire()
     {
-        Instantiate(projectile, transform);
+        GameObject gb = Instantiate(projectile, transform);
+        
     }
 }
