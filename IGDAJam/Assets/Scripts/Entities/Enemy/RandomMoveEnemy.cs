954f1fd17650c47ba3072d8ae9cc97b13a577012
Daniel Walls <walls.dk@gmail.com>
2022-01-30 11:14:22 -0800

    helper script, reorganizing

---
+++ IGDAJam/Assets/Scripts/Entities/Enemy/RandomMoveEnemy.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Entities.Enemy
+{
+    public class RandomMoveEnemy : MonoBehaviour
+    {
+        public State IdleState = new IdleState();
+        public State AttackState = new AttackState();
+
+        public State CurrentState;
+    }
+
+    public class IdleState : State
+    {
+        
+    }
+
+    public class AttackState : State
+    {
+        
+    }
+
+    public abstract class State
+    {
+        public virtual IEnumerator OnEnter()
+        {
+            yield return null;
+        }
+    }
+}
