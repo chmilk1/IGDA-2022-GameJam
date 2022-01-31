e85486a292ee29b789edbce45231336462e38e05
Daniel Walls <walls.dk@gmail.com>
2022-01-30 17:50:00 -0800

    player sounds

---
+++ IGDAJam/Assets/Scripts/Entities/PlayerDamageUpdater.cs
@@ -0,0 +1,18 @@
+﻿using FMODUnity;
+using UnityEngine;
+
+namespace Entities
+{
+    public class PlayerDamageUpdater : MonoBehaviour
+    {
+        public StudioEventEmitter emitter;
+        public Health playerHealth;
+        public AnimationCurve curve;
+        
+        private void Update()
+        {
+            float healthPercent = Mathf.Clamp01((float) playerHealth.RemainingHitPoints / playerHealth.MaxHealth);
+            emitter.SetParameter("Health", curve.Evaluate(healthPercent));
+        }
+    }
+}
